80940dd73886d2149ddf6c40914c971943faafbd
DanhVinhLe <132262572+DanhVinhLe@users.noreply.github.com>
2024-12-22 00:58:42 +0700

    fix greedy

--- algorithms/greedy.py
+++ algorithms/greedy.py
@@ -1,13 +1,19 @@
 import osmnx as ox 
 import networkx as nx
 import json
-
+from math import radians, cos, sin, asin, sqrt
 # Greedy Best First Search
 # heuristic function to estimate the cost from the current node to the goal node
 def heuristic(graph, node, end_node):
     node_x, node_y = graph.nodes[node]['x'], graph.nodes[node]['y']
     goal_x, goal_y = graph.nodes[end_node]['x'], graph.nodes[end_node]['y']
-    return ((node_x - goal_x) ** 2 + (node_y - goal_y) ** 2) ** 0.5
+    lon1, lat1, lon2, lat2 = map(radians, [node_x, node_y, goal_x, goal_y])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    r = 6371000
+    return c * r
 
 def greedy_best_first_search(graph, start_node, end_node, heuristic):
     open_list = [(start_node, 0)]
